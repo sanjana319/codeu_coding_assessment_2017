2a6f76856358cd481514212c9c31d66c45199ad4
sanjana319 <sanjana.natraj319@gmail.com>
2017-04-17 21:06:48 -0700

    Update MyJSONParser.java

--- src/com/google/codeu/codingchallenge/MyJSONParser.java
+++ src/com/google/codeu/codingchallenge/MyJSONParser.java
@@ -21,6 +21,85 @@ final class MyJSONParser implements JSONParser {
   @Override
   public JSON parse(String in) throws IOException {
     // TODO: implement this
-    return new MyJSON();
+		JSON j = new MyJSON(); //the object to be returned 
+		String s = in; //temp string to mess with and check for input
+		String tempchar, tempkey, tempvalue; //temp stuff for s 
+
+		int index = 0;
+		int end, next; 
+		//use these guys to parse through the input in
+
+		/*String temp = in.trim();
+		String first = temp.charAt(0);
+		String last = temp.charAt(temp.length() - 1);
+
+		if(!first.equals("{") || !last.equals("}")){
+			throw new IOException; 
+		}*/
+
+		for(int i = 0; i < in.length(); i++){ 
+			tempchar = in.substring(i, i+1); //checking if the first character of in is valid
+			if(tempchar.equals("{")){
+				end = in.indexOf("}")--; //if it has a {, then find the index of } and assign it
+				s = in.substring(i++);
+				
+				while(index < s.length()){
+					tempchar = s.substring(index, index++);
+					if(tempchar.equals(" ")){
+						index++;
+					} //parse through the input 
+
+					 //break if you reach } bc you've reached the end
+
+					if(tempchar.equals("}")){break;}
+
+					else if(tempchar.equals("\"")){
+						s = s.substring(index++);
+						index = s.indexOf("\"");
+						tempkey = s.substring(0, index);
+						s = s.substring(index++);
+						index = 0;
+						next = s.indexOf("\"");
+
+						if(s.contains(",") == true){
+							index = s.indexOf(",");
+							s = s.substring(index++);
+							index = 0;
+						}
+
+						else{
+							in = "";
+							break;
+						}
+
+						String c = s.substring(index, next);
+
+						if(c.contains("{")){
+							int endval = s.indexOf("}");
+							String val = s.substring(0, endval+++);
+							JSON v = parse(val);
+							j.setObject(key, v);
+							s = s.substring(endval++);
+						}
+
+						else{
+							s=s.substring(next++);
+							index = s.indexOf("\"");
+							tempvalue = s.substring(0, index);
+							j.setString(key, value);
+						}
+
+
+
+						if(!s.contains(":")){
+							throw new IOException;
+						}
+					}
+
+					}
+				}
+			}
+		}
+		return j;
+	}
   }
-}
